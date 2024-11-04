120b416e6c56822c9db11e8a81517c00a67b6e14
Alexander Merino <alexander.merino@finerioconnect.com>
2024-11-04 14:25:03 -0500

    fix: changue pipeline to push heroku

--- src/api/components/auth/authService.js
+++ src/api/components/auth/authService.js
@@ -3,14 +3,18 @@ const jwt = require('jsonwebtoken')
 
 
 exports.login = async ({ phone, password }) => {
+  try {
 
-  const token = jwt.sign({ phone, password }, secret, { expiresIn: time })
+    const token = jwt.sign({ phone, password }, secret, { expiresIn: time })
 
-  console.log(`Se ha generado un token para el usuario ${phone}`)
+    console.log(`Se ha generado un token para el usuario ${phone}`)
 
-  return {
-    token: token
+    return {
+      token: token
+    }
+  } catch (error) {
+    console.log(error)
+    throw new Error('Error al generar token')
   }
-
 };
 
